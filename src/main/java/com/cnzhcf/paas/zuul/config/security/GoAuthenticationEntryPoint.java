45278740dc19ae4f7213439877a9ec60c5cd080f
ang0709 <920397317@qq.com>
2019-04-02 16:12:14 +0800

    调试

--- src/main/java/com/cnzhcf/paas/zuul/config/security/GoAuthenticationEntryPoint.java
+++ src/main/java/com/cnzhcf/paas/zuul/config/security/GoAuthenticationEntryPoint.java
@@ -3,6 +3,8 @@ package com.cnzhcf.paas.zuul.config.security;
 
 import com.cnzhcf.paas.commons.util.FastJsonUtil;
 import com.cnzhcf.paas.commons.util.ZhJsonResult;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -17,11 +19,16 @@ import java.io.IOException;
  * @Date:Created in 2018/8/25 23:11.
  */
 public class GoAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    protected final Log logger = LogFactory.getLog(this.getClass());
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
 //
         response.setHeader("Content-Type", "application/json;charset=utf-8");
         ZhJsonResult jsonResult = new ZhJsonResult();
+        logger.debug("############################  401" + request.getRequestURI());
+        logger.info("############################  401" + request.getRequestURI());
+        logger.error("############################  401" + request.getRequestURI());
         response.setStatus(401);
         response.getWriter().write(FastJsonUtil.bean2Json(jsonResult.tail("未登录"), true));
         response.getWriter().flush();
