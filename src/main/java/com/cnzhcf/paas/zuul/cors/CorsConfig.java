29df609da9197646417f619e5af90fdcc75d8e6d
ang0709 <920397317@qq.com>
2019-05-08 17:34:24 +0800

    nosubmit

--- src/main/java/com/cnzhcf/paas/zuul/cors/CorsConfig.java
+++ src/main/java/com/cnzhcf/paas/zuul/cors/CorsConfig.java
@@ -1,22 +1,25 @@
 package com.cnzhcf.paas.zuul.cors;
 
 
+import org.openxmlformats.schemas.drawingml.x2006.chart.CTBarSer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
+@Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
-   /* private CorsConfiguration buildConfig() {
+    private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addExposedHeader("Authorization");
+      //  corsConfiguration.addExposedHeader("Authorization");
         return corsConfiguration;
     }
 
@@ -24,10 +27,12 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig());
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return new CorsFilter(source);
     }
 
-    @Override
+  /*  @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
